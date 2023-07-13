557bbbbe9fc9456a698f2009a9b15d80d30309e5
philanthropistaria <aatandoh56@gmail.com>
2023-07-13 14:31:13 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-/**
- * main - printing a line of code with the put function
- * Return: 0(success)
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>                                                                  
+/**                                                                                 
+ * Description: main - Prints out a string to stdout.                            
+ * Return: 0 if success.                                                        
+ */                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("with proper grammar, but the outcome is a piece of art,\n");                
 return (0);
 }
