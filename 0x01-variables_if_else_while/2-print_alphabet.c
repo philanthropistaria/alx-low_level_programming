1982066cccaba0fe3b9d8335cdb14e2a3dd52fb0
philanthropistaria <aatandoh56@gmail.com>
2023-07-14 09:46:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets from a to z
+ * Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+{
+	putchar (letter);
+}
+putchar ('\n');
+	return (0);
+}
