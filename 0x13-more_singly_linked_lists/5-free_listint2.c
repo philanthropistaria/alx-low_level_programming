852ad28cda52eac81af810b7dace0889863f0b3a
philanthropistaria <aatandoh56@gmail.com>
2023-08-28 12:57:25 -0700

    snappy

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - will free the linked list
+ * @head: the pointer to the list is to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
+
